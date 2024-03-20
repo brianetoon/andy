8fcfb0a277f48a8abd26f4e2ad0db407e6421f86
Brian <brian.elliot.toon@gmail.com>
2024-03-20 15:02:44 -0400

    hash history mode & gh-pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import ProjectDetails from '../views/ProjectDetails.vue'
 import store from '../store.js'
@@ -32,7 +32,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  // history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior() {
     return new Promise((resolve) => {
